7cc83a733a36c4a9aa65f7ce025636b0ebfb4cfd
SajedaJomaa <sajedajomaa57@gmail.com>
2024-07-11 21:05:03 +0300

    Place Picker Project

---
+++ src/http.js
@@ -0,0 +1,37 @@
+export async function fetchAvailablePlaces() {
+    const response = await fetch('http://localhost:3000/places');
+    const resData = await response.json();
+
+    if (!response.ok) {//400,500
+        throw new Error('Faild to fetch palces');
+    }
+    return resData.places;
+}
+
+
+export async function fetchUserPlaces() {
+    const response = await fetch('http://localhost:3000/user-places');
+    const resData = await response.json();
+
+    if (!response.ok) {//400,500
+        throw new Error('Faild to fetch user palces');
+    }
+    return resData.places;
+}
+
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({ places: places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    });
+    const resData = await response.json();
+    if (!response.ok) {
+        throw new Error('Faild to update user data.');
+    }
+    return resData.message;
+}
